d321b4b61c91ec2995e39bebb0ab5174bcc61fed
LucasBoTang <lucastang1994@gmail.com>
2019-05-06 23:09:39 +0200

    Add solver

---
+++ solver.py
@@ -0,0 +1,94 @@
+from model import Encoder, Transformer, Reconstructor, Discriminator
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import torch
+import torch.nn.functional as F
+import numpy as np
+import os
+import time
+import datetime
+
+
+class Solver(object):
+    """
+    solver for training and testing ModularGAN
+    """
+
+    def __init__(self, data_loader, config):
+        """
+        initialize configurations from argument
+        """
+        # data loader
+        self.data_loader = data_loader
+
+        # model configurations
+        self.image_size = config.image_size
+        self.e_conv_dim = config.e_conv_dim
+        self.r_conv_dim = config.r_conv_dim
+        self.e_repeat_num = config.e_repeat_num
+        self.t_repeat_num = config.t_repeat_num
+        self.d_repeat_num = config.d_repeat_num
+        self.lambda_cls = config.lambda_cls
+        self.lambda_rec = config.lambda_rec
+        self.lambda_gp = config.lambda_gp
+        self.attr_dims = config.attr_dims
+        self.selected_attrs = config.selected_attrs
+
+        # training configurations
+        self.batch_size = config.batch_size
+        self.num_iters = config.num_iters
+        self.num_iters_decay = config.num_iters_decay
+        self.g_lr = config.g_lr
+        self.d_lr = config.d_lr
+        self.n_critic = config.n_critic
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.resume_iters = config.resume_iters
+
+        # test configurations
+        self.test_iters = config.test_iters
+
+        # miscellaneous.
+        self.use_tensorboard = config.use_tensorboard
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # directories
+        self.log_dir = config.log_dir
+        self.sample_dir = config.sample_dir
+        self.model_save_dir = config.model_save_dir
+        self.result_dir = config.result_dir
+
+        # step size
+        self.log_step = config.log_step
+        self.sample_step = config.sample_step
+        self.model_save_step = config.model_save_step
+        self.lr_update_step = config.lr_update_step
+
+        # build the model and tensorboard
+        self.build_model()
+        #if self.use_tensorboard:
+        #    self.build_tensorboard()
+
+    def build_model(self):
+        """
+        create network modulars
+        """
+        # create modulars
+        self.E = Encoder(conv_dim=self.e_conv_dim, repeat_num=self.e_repeat_num)
+        self.E.to(self.device)
+
+        self.T = torch.nn.ModuleList()
+        for c_dim in self.attr_dims:
+            self.T.append(Transformer(conv_dim=self.e_conv_dim*4, c_dim=c_dim, repeat_num=self.t_repeat_num))
+        self.T.to(self.device)
+
+        self.R = Reconstructor(conv_dim=self.r_conv_dim)
+        self.R.to(self.device)
+
+        self.D = torch.nn.ModuleList()
+        for c_dim in self.attr_dims:
+            self.D.append(Discriminator(image_size=self.image_size, conv_dim=self.r_conv_dim, c_dim=c_dim, repeat_num=self.r_conv_dim))
+        self.D.to(self.device)
+
+        self.g_optimizer = torch.optim.Adam(list(self.E.parameters())+list(self.T.parameters())+list(self.R.parameters()), self.g_lr, [self.beta1, self.beta2])
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), self.d_lr, [self.beta1, self.beta2])
